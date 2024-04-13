42e6535330146b6fc212e2c04b889d60754f3c6d
Lukas Starenko <31926276+Lukysoon@users.noreply.github.com>
2024-04-13 17:26:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,43 +10,31 @@ cwd=$(pwd)
 FAIRSEQ=${cwd}/fairseq/fairseq
 CODE=${cwd}
 
-if [ $stage -le 0 ] && [ $stop_stage -ge 0 ]; then
-    echo "Stage 0: Install conda environment..."
+echo "Stage 0: Install conda environment..."
     
-    conda create --name ${PYTHON_ENVIRONMENT} python=3.7 -y
-fi
+conda create --name ${PYTHON_ENVIRONMENT} python=3.7 -y
+
 
 conda activate ${PYTHON_ENVIRONMENT}
 
-if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
-    echo "Stage 1: Install fairseq and other dependencies..."
+echo "Stage 1: Install fairseq and other dependencies..."
     
-    if [ ! -d fairseq ]; then
-        git clone https://github.com/pytorch/fairseq.git --branch main --single-branch
-    fi
-    cd ${cwd}/fairseq
+git clone https://github.com/pytorch/fairseq.git --branch main --single-branch
+cd ${cwd}/fairseq
     # checkout the fairseq version to use
-    git reset --hard 0b21875e45f332bedbcc0617dcf9379d3c03855f
+git reset --hard 0b21875e45f332bedbcc0617dcf9379d3c03855f
 
-    if [ $(pip freeze | grep fairseq | wc -l ) -gt 0 ]; then
-        echo "Already installed fairseq. Skip..."
-    else
-        echo "Install fairseq..."
-        python -m pip install --editable ./
-    fi
-    # optionally do this if related error occurs
-    python setup.py build_ext --inplace
-    
-    python -m pip install scipy
-    python -m pip install soundfile
-    python -m pip install praat-parselmouth
-    python -m pip install tensorboardX
-    
-    cd ${cwd}
-fi
+echo "Install fairseq..."
+python -m pip install --editable ./
 
-if [ $stage -le 2 ] && [ $stop_stage -ge 2 ]; then
-    echo "Stage 2: Copy model files to fairseq..."
-    rsync -a contentvec/ fairseq/fairseq/
+apt update
+apt isntall rsync -y
+python -m pip install scipy
+python -m pip install soundfile
+python -m pip install praat-parselmouth
+python -m pip install tensorboardX
     
-fi
+cd ${cwd}
+
+echo "Stage 2: Copy model files to fairseq..."
+rsync -a contentvec/ fairseq/fairseq/
